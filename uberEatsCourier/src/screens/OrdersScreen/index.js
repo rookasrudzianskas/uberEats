f2f99efed7a0a7967dfafcd3691bc87019018e55
Rokas <rookas.rudzenskas@gmail.com>
2022-10-13 20:55:18 +0300

    Rokas

--- uberEatsCourier/src/screens/OrdersScreen/index.js
+++ uberEatsCourier/src/screens/OrdersScreen/index.js
@@ -1,14 +1,17 @@
 import React, {useRef} from 'react';
 import {Text, View, StyleSheet} from 'react-native';
+import BottomSheet from "@gorhom/bottom-sheet";
 
 const OrdersScreen = () => {
     const bottomSheetRef = useRef(null);
 
     return (
         <View>
-            <Text>
-                byrookas 🚀
-            </Text>
+            <BottomSheet ref={bottomSheetRef} snapPoints={["12%", "95%"]}>
+                <View className="bg-red-500 flex-1">
+                    <Text>Hello</Text>
+                </View>
+            </BottomSheet>
         </View>
     );
 };
