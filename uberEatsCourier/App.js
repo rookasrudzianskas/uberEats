7f82a5bd3d5d04df7538c5d729347a6230e565ba
Rokas <rookas.rudzenskas@gmail.com>
2022-10-13 20:21:14 +0300

    Rokas

--- uberEatsCourier/App.js
+++ uberEatsCourier/App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import {Image, StyleSheet, Text, View} from 'react-native';
+import {Image, StyleSheet, Text, TouchableOpacity, View} from 'react-native';
 import orders from './src/assets/data/orders.json';
 import {Entypo} from "@expo/vector-icons";
 
@@ -8,20 +8,22 @@ export default function App() {
 
     return (
         <View className="mt-10 mx-4">
-            <View>
-                <Image source={{uri: order?.Restaurant?.image}} className="w-20 h-28" />
-                <View>
-                    <Text className="text-lg font-bold">{order?.Restaurant?.name}</Text>
-                    <Text>{order?.Restaurant?.address}</Text>
-                    <Text>Delivery Details:</Text>
+            <TouchableOpacity activeOpacity={0.7} className="flex-row items-center border border-[2px] border-green-500 rounded-lg">
+                <View className="flex-1 flex-row">
+                    <Image source={{uri: order?.Restaurant?.image}} style={{}} className="w-20 m-1 rounded-lg" />
+                    <View className="px-3 py-3">
+                        <Text className="text-lg font-bold max-w-[200px]" style={{lineHeight: 25}}>{order?.Restaurant?.name}</Text>
+                        <Text className="text-sm text-gray-500">{order?.Restaurant?.address}</Text>
+                        <Text className="text-[15px] font-[600] mt-[4px]">Delivery Details:</Text>
 
-                    <Text>{order?.User?.name}</Text>
-                    <Text>{order?.User?.address}</Text>
+                        <Text className="text-sm text-gray-500 mt-[2px]">{order?.User?.name}</Text>
+                        <Text className="text-sm text-gray-500">{order?.User?.address}</Text>
+                    </View>
                 </View>
-                <View>
-                    <Entypo name="check" size={30} color="black" />
+                <View className="bg-green-500 h-full flex-row px-2 items-center justify-center">
+                    <Entypo name="check" size={30} color="white" />
                 </View>
-            </View>
+            </TouchableOpacity>
             <StatusBar style="auto" />
         </View>
     );
