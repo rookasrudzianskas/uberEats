6a4bb60c79f01b8f8c5c12dc462f626474e519bc
Rokas <rookas.rudzenskas@gmail.com>
2022-10-13 21:25:00 +0300

    Rokas

--- uberEatsCourier/src/screens/OrderDelivery/index.js
+++ uberEatsCourier/src/screens/OrderDelivery/index.js
@@ -3,11 +3,12 @@ import {Text, View, StyleSheet, FlatList} from 'react-native';
 import BottomSheet from "@gorhom/bottom-sheet";
 import orders from "../../assets/data/orders.json";
 import OrderItem from "../../components/OrderItem";
-import {Ionicons} from "@expo/vector-icons";
+import {FontAwesome, Ionicons} from "@expo/vector-icons";
 
 const OrderDelivery = () => {
     const bottomSheetRef = useRef(null);
     const snapPoints = useMemo(() => ["12%", "95%"], []);
+    const order = orders[0];
 
     return (
         <View className="bg-gray-100 h-screen">
@@ -19,8 +20,26 @@ const OrderDelivery = () => {
                         <Text className="text-[19px] text-gray-900 font-[500]">3.087 km</Text>
                     </View>
                     <View className="border-b border-gray-300 border-[2px]"/>
-                    <View>
-                        <Text>El Cabo Coffe Bar Tres De Mayo</Text>
+                    <View className="mx-4">
+                        <Text className="text-2xl text-gray-900 font-[500] mt-8">{order?.Restaurant?.name}</Text>
+                        <View className="mt-4 space-y-4">
+                            <View className="flex-row items-center space-x-3">
+                                <FontAwesome name="building-o" size={24} color="gray" />
+                                <Text className="text-[20px] text-gray-600 font-[400]">{order?.Restaurant?.address}</Text>
+                            </View>
+                            <View className="flex-row items-center space-x-3 pb-4">
+                                <FontAwesome name="map-marker" size={24} color="gray" />
+                                <Text className="text-[20px] text-gray-600 font-[400]">{order?.User?.address}</Text>
+                            </View>
+                            <View className="border-b border-gray-300 border-[1px]"/>
+
+                            <View className="space-y-1">
+                                <Text className="text-[17px] font-[600] text-gray-500">Onion Rings x1</Text>
+                                <Text className="text-[17px] font-[600] text-gray-500">Big Mac x2</Text>
+                                <Text className="text-[17px] font-[600] text-gray-500">Big Tasty x1</Text>
+                                <Text className="text-[17px] font-[600] text-gray-500">Coca Cola x4</Text>
+                            </View>
+                        </View>
                     </View>
                 </View>
             </BottomSheet>
