5b4332d4f3f7f53ed36c3e7ef8b8642ea6cd9da8
Rokas <rookas.rudzenskas@gmail.com>
2022-10-13 20:09:18 +0300

    Roaks

--- uberEatsCourier/App.js
+++ uberEatsCourier/App.js
@@ -1,12 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {Image, StyleSheet, Text, View} from 'react-native';
 import orders from './src/assets/data/orders.json';
 
 export default function App() {
+    const order = orders[0];
+
     return (
-        <View className="">
+        <View className="mt-10 mx-4">
             <View>
-                <Text className="text-lg mt-10 font-bold">{orders[0]?.Restaurant?.name}</Text>
+                <Image source={{uri: order?.Restaurant?.image}} className="w-20 h-28" />
+                <View>
+                    <Text className="text-lg font-bold">{order?.Restaurant?.name}</Text>
+                    <Text>{order?.Restaurant?.address}</Text>
+                    <Text>Delivery Details:</Text>
+
+                    <Text>{order?.User?.name}</Text>
+                    <Text>{order?.User?.address}</Text>
+                </View>
+                <View>
+                    {/*<Entypo name="check" size={30} color="black" />*/}
+                </View>
             </View>
             <StatusBar style="auto" />
         </View>
