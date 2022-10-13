43dea3321914e4a0dff9864da1fee457bef62ec5
Rokas <rookas.rudzenskas@gmail.com>
2022-10-13 21:14:31 +0300

    Rokas

--- uberEatsCourier/src/screens/OrderDelivery/index.js
+++ uberEatsCourier/src/screens/OrderDelivery/index.js
@@ -11,16 +11,16 @@ const OrderDelivery = () => {
 
     return (
         <View className="bg-gray-100 h-screen">
-            <BottomSheet ref={bottomSheetRef} index={1} snapPoints={snapPoints}>
+            <BottomSheet handleIndicatorStyle={{backgroundColor: 'grey', width: 100}} ref={bottomSheetRef} index={1} snapPoints={snapPoints}>
                 <View className="">
-                    <View className="flex-row items-center justify-center mt-3 mb-6 mx-4">
+                    <View className="flex-row items-center justify-center mt-3 mb-6 mx-4 space-x-1">
                         <Text className="text-[19px] text-gray-900 font-[500]">14 min</Text>
                         <Ionicons name="basket" size={27} color="green" />
                         <Text className="text-[19px] text-gray-900 font-[500]">3.087 km</Text>
                     </View>
                     <View className="border-b border-gray-300 border-[2px]"/>
                     <View>
-
+                        <Text>El Cabo Coffe Bar Tres De Mayo</Text>
                     </View>
                 </View>
             </BottomSheet>
