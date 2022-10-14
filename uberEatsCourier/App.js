aa3f1b9ea9ffd26aba6ab0534bce6f21785e622d
Rokas <rookas.rudzenskas@gmail.com>
2022-10-14 08:59:19 +0300

    Rokas

--- uberEatsCourier/App.js
+++ uberEatsCourier/App.js
@@ -2,16 +2,15 @@ import { StatusBar } from 'expo-status-bar';
 import {FlatList, Image, StyleSheet, Text, TouchableOpacity, View} from 'react-native';
 import OrderDelivery from "./src/screens/OrderDelivery";
 import { NavigationContainer } from '@react-navigation/native';
+import RootNavigator from "./src/navigation";
 
 export default function App() {
 
     return (
-        <View className="">
-            <NavigationContainer>
-                <OrderDelivery />
-                <StatusBar style="auto" />
-            </NavigationContainer>
-        </View>
+        <NavigationContainer>
+            <RootNavigator />
+            <StatusBar style="auto" />
+        </NavigationContainer>
     );
 }
 
