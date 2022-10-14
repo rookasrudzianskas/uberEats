dd730fa78b74f480fa3c51b961140cae6d1f2bc6
Rokas <rookas.rudzenskas@gmail.com>
2022-10-14 16:09:36 +0300

    Rokas

--- uberEatsCourier/App.js
+++ uberEatsCourier/App.js
@@ -5,7 +5,6 @@ import "react-native-gesture-handler";
 import {GestureHandlerRootView} from "react-native-gesture-handler";
 
 const App = () => {
-
     return (
         <GestureHandlerRootView style={{flex: 1}}>
             <NavigationContainer>
