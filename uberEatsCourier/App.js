d52b41209a1ef2c6f758acf4b0335f9e7423c8f2
Rokas <rookas.rudzenskas@gmail.com>
2022-10-14 08:57:00 +0300

    Rokas

--- uberEatsCourier/App.js
+++ uberEatsCourier/App.js
@@ -1,23 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import {FlatList, Image, StyleSheet, Text, TouchableOpacity, View} from 'react-native';
-import orders from './src/assets/data/orders.json';
-import {Entypo} from "@expo/vector-icons";
-import OrderItem from "./src/components/OrderItem";
-import OrdersScreen from "./src/screens/OrdersScreen";
 import OrderDelivery from "./src/screens/OrderDelivery";
+import { NavigationContainer } from '@react-navigation/native';
 
 export default function App() {
 
     return (
         <View className="">
-            {/*<FlatList data={orders}*/}
-            {/*          keyExtractor={item => item.id}*/}
-            {/*          renderItem={({item}) => (*/}
-            {/*    <OrderItem order={item} />*/}
-            {/*)} />*/}
-            {/*<OrdersScreen />*/}
-            <OrderDelivery />
-            <StatusBar style="auto" />
+            <NavigationContainer>
+                <OrderDelivery />
+                <StatusBar style="auto" />
+            </NavigationContainer>
         </View>
     );
 }
