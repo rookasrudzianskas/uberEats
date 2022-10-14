2b48af3350aba835985429d90d3a8238a0616038
Rokas <rookas.rudzenskas@gmail.com>
2022-10-14 09:08:21 +0300

    Rokas

--- uberEatsCourier/src/navigation/index.js
+++ uberEatsCourier/src/navigation/index.js
@@ -1,17 +1,7 @@
 import * as React from 'react';
-import { View, Text } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import OrdersScreen from "../screens/OrdersScreen";
 import OrderDelivery from "../screens/OrderDelivery";
-
-function HomeScreen() {
-    return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Text>Home Screen</Text>
-        </View>
-    );
-}
 
 const Stack = createNativeStackNavigator();
 
