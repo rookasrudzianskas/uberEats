10ede647028a8f51b7d65f737db84205817629cd
Rokas <rookas.rudzenskas@gmail.com>
2022-10-14 15:34:16 +0300

    Rokas

--- uberEatsCourier/App.js
+++ uberEatsCourier/App.js
@@ -3,14 +3,18 @@ import {FlatList, Image, StyleSheet, Text, TouchableOpacity, View} from 'react-n
 import OrderDelivery from "./src/screens/OrderDelivery";
 import { NavigationContainer } from '@react-navigation/native';
 import Navigation from "./src/navigation";
+import "react-native-gesture-handler";
+import {GestureHandlerRootView} from "react-native-gesture-handler";
 
 export default function App() {
 
     return (
-        <NavigationContainer>
-            <Navigation />
-            <StatusBar style="auto" />
-        </NavigationContainer>
+        <GestureHandlerRootView style={{flex: 1}}>
+            <NavigationContainer>
+                <Navigation />
+                <StatusBar style="auto" />
+            </NavigationContainer>
+        </GestureHandlerRootView>
     );
 }
 
