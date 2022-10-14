c7bfefd921ddf15f716f85ae663b2324f23ca90f
Rokas <rookas.rudzenskas@gmail.com>
2022-10-14 16:52:14 +0300

    Rokas

--- uberEatsCourier/src/contexts/AuthContext.js
+++ uberEatsCourier/src/contexts/AuthContext.js
@@ -1,13 +1,13 @@
 // create context
 import {createContext, useContext, useEffect, useState} from "react";
 import {Auth, DataStore} from "aws-amplify";
-import {User} from "../models";
+import {Courier, User} from "../models";
 
 const AuthContext = createContext({});
 
 const AuthContextProvider = ({children}) => {
     const [authUser, setAuthUser] = useState(null);
-    const [dbUser, setDbUser] = useState(null);
+    const [dbCourier, setDbCourier] = useState(null);
 
     useEffect(() => {
         Auth.currentAuthenticatedUser({bypassCache: true}).then(setAuthUser);
@@ -16,15 +16,15 @@ const AuthContextProvider = ({children}) => {
     const sub = authUser?.attributes?.sub;
 
     useEffect(() => {
-        DataStore.query(User, (user) => user.sub('eq', sub)).then((users) => setDbUser(users[0]));
+        DataStore.query(Courier, (courier) => courier.sub('eq', sub)).then((couriers) => setDbCourier(couriers[0]));
     }, [sub]);
 
     return (
         <AuthContext.Provider value={{
             authUser,
-            dbUser,
+            dbCourier,
             sub,
-            setDbUser
+            setDbCourier
         }}>
             {children}
         </AuthContext.Provider>
