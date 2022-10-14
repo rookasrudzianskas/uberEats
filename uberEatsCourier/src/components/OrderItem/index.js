ebb8430b65591920c1e3ef06cfdbfe2e6a08bcac
Rokas <rookas.rudzenskas@gmail.com>
2022-10-14 17:35:21 +0300

    Rokas

--- uberEatsCourier/src/components/OrderItem/index.js
+++ uberEatsCourier/src/components/OrderItem/index.js
@@ -1,10 +1,18 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {Text, View, StyleSheet, Image, TouchableOpacity} from 'react-native';
 import {Entypo} from "@expo/vector-icons";
 import {useNavigation} from "@react-navigation/native";
+import {DataStore} from "aws-amplify";
+import {User} from "../../models";
 
 const OrderItem = ({order}) => {
     const navigation = useNavigation();
+    const [user, setUser] = useState(null);
+    // console.log(order);
+
+    useEffect(() => {
+        DataStore.query(User, order.userID).then(setUser);
+    }, []);
 
     return (
         <TouchableOpacity onPress={() => navigation.navigate('OrdersDeliveryScreen', {id: order.id})} activeOpacity={0.7} className="flex-row items-center border border-[2px] my-2 border-green-500 rounded-lg">
@@ -15,8 +23,8 @@ const OrderItem = ({order}) => {
                     <Text className="text-sm text-gray-500">{order?.Restaurant?.address}</Text>
                     <Text className="text-[15px] font-[600] mt-[4px]">Delivery Details:</Text>
 
-                    <Text className="text-sm text-gray-500 mt-[2px]">{order?.User?.name || 'Loading...'}</Text>
-                    <Text className="text-sm text-gray-500">{order?.User?.address || 'Loading...'}</Text>
+                    <Text className="text-sm text-gray-500 mt-[2px]">{user?.name || 'Loading...'}</Text>
+                    <Text className="text-sm text-gray-500">{user?.address || 'Loading...'}</Text>
                 </View>
             </View>
             <View className="bg-green-500 h-full flex-row px-2 items-center justify-center">
