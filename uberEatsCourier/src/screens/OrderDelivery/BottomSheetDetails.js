4dddcefad917568912af915810883c7ff1476009
Rokas <rookas.rudzenskas@gmail.com>
2022-10-15 20:42:05 +0300

    Rokas

---
+++ uberEatsCourier/src/screens/OrderDelivery/BottomSheetDetails.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Text, View, StyleSheet} from 'react-native';
+
+const BottomSheetDetails = () => {
+    return (
+        <View>
+            <Text>
+                byrookas 🚀
+            </Text>
+        </View>
+    );
+};
+
+export default BottomSheetDetails;
