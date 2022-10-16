4f695626fdf520d1d8f060631725bc85e1ccd3db
Rokas <rookas.rudzenskas@gmail.com>
2022-10-16 18:20:55 +0300

    Rokas

--- uberEats/src/screens/OrderLiveUpdates/index.js
+++ uberEats/src/screens/OrderLiveUpdates/index.js
@@ -1,5 +1,5 @@
-import React, {useEffect, useState} from 'react';
-import {Text, View, StyleSheet} from 'react-native';
+import React, {useEffect, useRef, useState} from 'react';
+import {Text, View, StyleSheet, TouchableOpacity} from 'react-native';
 import MapView, {Marker} from 'react-native-maps';
 import {DataStore} from "aws-amplify";
 import {Courier, Order} from "../../models";
@@ -8,6 +8,7 @@ import {FontAwesome5} from "@expo/vector-icons";
 const OrderLiveUpdates = ({id}) => {
     const [order, setOrder] = useState(null);
     const [courier, setCourier] = useState(null);
+    const mapRef = useRef(null);
 
     useEffect(() => {
         DataStore.query(Order, id).then(setOrder);
@@ -19,11 +20,22 @@ const OrderLiveUpdates = ({id}) => {
         }
     }, [order]);
 
+    useEffect(() => {
+        if(courier?.lng && courier?.lat) {
+            mapRef.current.animateToRegion({
+                latitude: courier?.lat,
+                longitude: courier?.lng,
+                latitudeDelta: 0.007,
+                longitudeDelta: 0.007
+            })
+        }
+    }, [courier?.lng, courier?.lat]);
+
 
     return (
         <View>
             <Text className="text-center text-xl bg-transparent py-1 font-semibold">Status: {order?.status || 'Loading...'}</Text>
-            <MapView className="h-full w-full" >
+            <MapView ref={mapRef} className="h-full w-full" >
                 {courier?.lat && (
                     <Marker
                         coordinate={{
@@ -31,7 +43,9 @@ const OrderLiveUpdates = ({id}) => {
                             longitude: courier?.lng,
                         }}
                     >
-                        <FontAwesome5 name="motorcycle" size={24} color={"black"} />
+                        <TouchableOpacity activeOpacity={0.7} className="bg-green-500 rounded-full w-10 h-10 items-center justify-center">
+                            <FontAwesome5 name="motorcycle" size={22} color={"white"} />
+                        </TouchableOpacity>
                     </Marker>
                 )}
             </MapView>
