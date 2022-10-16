4beef77e3941457522de1c02d2aa01d6c162167b
Rokas <rookas.rudzenskas@gmail.com>
2022-10-16 11:04:24 +0300

    Rokas

---
+++ uberEatsCourier/src/screens/OrderDelivery/CustomMarker.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {Text, View, StyleSheet, TouchableOpacity} from 'react-native';
+import {Entypo} from "@expo/vector-icons";
+import {Marker} from "react-native-maps";
+
+const CustomMarker = () => {
+    return (
+        <Marker
+            coordinate={{latitude: order?.Restaurant?.lat, longitude: order?.Restaurant?.lng}}
+            title={order?.Restaurant?.name}
+            description={order?.Restaurant?.address}
+        >
+            <TouchableOpacity activeOpacity={0.7} className="bg-green-500 p-1 rounded-full">
+                <Entypo name="shop" size={22} color="white" />
+            </TouchableOpacity>
+        </Marker>
+    );
+};
+
+export default CustomMarker;
