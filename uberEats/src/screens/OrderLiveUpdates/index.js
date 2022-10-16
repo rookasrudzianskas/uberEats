3fec5f51989d3f30d7cb5a3a9446ecb9e131dc85
Rokas <rookas.rudzenskas@gmail.com>
2022-10-16 17:05:22 +0300

    Rokas

--- uberEats/src/screens/OrderLiveUpdates/index.js
+++ uberEats/src/screens/OrderLiveUpdates/index.js
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Text, View, StyleSheet} from 'react-native';
 import MapView from 'react-native-maps';
 
-const OrderLiveUpdates = () => {
+const OrderLiveUpdates = ({id}) => {
+    const [order, setOrder] = useState(null);
+
     return (
         <View>
             <MapView className="h-full w-full" />
