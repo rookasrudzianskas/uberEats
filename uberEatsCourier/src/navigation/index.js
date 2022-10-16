dd695178d8a85426895a2ade55bb684ff61f3449
Rokas <rookas.rudzenskas@gmail.com>
2022-10-16 14:03:02 +0300

    Rokas

--- uberEatsCourier/src/navigation/index.js
+++ uberEatsCourier/src/navigation/index.js
@@ -4,11 +4,19 @@ import OrdersScreen from "../screens/OrdersScreen";
 import OrderDelivery from "../screens/OrderDelivery";
 import ProfileScreen from "../screens/ProfileScreen";
 import {useAuthContext} from "../contexts/AuthContext";
+import {ActivityIndicator, View} from "react-native";
 
 const Stack = createNativeStackNavigator();
 
 const Navigation = () => {
     const {dbCourier} = useAuthContext();
+    if(dbCourier === null) {
+        return (
+            <View className="bg-gray-100 h-screen justify-center items-center">
+                <ActivityIndicator />
+            </View>
+        )
+    }
     return (
         <Stack.Navigator>
             {dbCourier ? (
