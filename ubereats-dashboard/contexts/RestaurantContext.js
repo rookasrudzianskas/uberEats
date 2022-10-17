8e710d8637ff191825961a8ba9bbcfde85fdc3de
Rokas <rookas.rudzenskas@gmail.com>
2022-10-17 16:13:03 +0300

    Rokas

--- ubereats-dashboard/contexts/RestaurantContext.js
+++ ubereats-dashboard/contexts/RestaurantContext.js
@@ -1,7 +1,14 @@
 // create Restaurant Context
 import { createContext } from "react";
 
-const RestaurantContext = createContext();
+const RestaurantContext = createContext({});
 
+const RestaurantContextProvider = ({ children }) => {
+    return (
+        <RestaurantContext.Provider>
+            {children}
+        </RestaurantContext.Provider>
+    )
+}
 
 export default RestaurantContext;
