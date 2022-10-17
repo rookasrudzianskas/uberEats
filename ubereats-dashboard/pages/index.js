7400cb7c6a7fdc32c8ef9e1828d7f01a66a96897
Rokas <rookas.rudzenskas@gmail.com>
2022-10-17 09:22:04 +0300

    Rokas

--- ubereats-dashboard/pages/index.js
+++ ubereats-dashboard/pages/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import {Card, Descriptions, Divider, List, Button} from 'antd';
 
 export default function Home() {
   return (
@@ -9,9 +10,9 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main>
+      <Card title={'Order Title'}>
 
-      </main>
+      </Card>
     </div>
   )
 }
