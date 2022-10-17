5649f5417518dcad710cfdf814ba8268931c9d3e
Rokas <rookas.rudzenskas@gmail.com>
2022-10-17 11:02:13 +0300

    Rokas

--- ubereats-dashboard/components/SideMenu.js
+++ ubereats-dashboard/components/SideMenu.js
@@ -1,19 +1,35 @@
 import React from 'react';
 import {Menu} from 'antd';
+import {useRouter} from "next/router";
 
 const SideMenu = ({}) => {
+    const router = useRouter();
+
     const menuItems = [
         {
             key: '/',
             label: 'Orders',
         },
         {
-            key: '/menu',
+            key: 'menu',
             label: 'Menu',
         },
-    ]
+        {
+            key: 'order-history',
+            label: 'Order History',
+        },
+        {
+            key: 'settings',
+            label: 'Settings',
+        }
+    ];
+
+    const onMenuItemClicked = (menuItem) => {
+        router.push(`/${menuItem?.key}`)
+    }
+
     return (
-        <Menu items={menuItems} />
+        <Menu items={menuItems} onClick={onMenuItemClicked} />
     );
 };
 
