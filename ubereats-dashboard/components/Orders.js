0a207f46e4700591cf3cee9f85442833fdcdfc92
Rokas <rookas.rudzenskas@gmail.com>
2022-10-17 17:34:04 +0300

    Rokas

--- ubereats-dashboard/components/Orders.js
+++ ubereats-dashboard/components/Orders.js
@@ -3,20 +3,25 @@ import {Card, Table, Tag} from 'antd';
 import {useRouter} from "next/router";
 import {DataStore} from "aws-amplify";
 import {Order, OrderStatus} from "../src/models";
+import {useRestaurantContext} from "../contexts/RestaurantContext";
 
 const Orders = () => {
     const [orders, setOrders] = useState([]);
+    const {restaurant} = useRestaurantContext();
 
     const fetchOrders = async () => {
-        const orders = await DataStore.query(Order);
+        const orders = await DataStore.query(Order, (order) =>
+            order.orderRestaurantId("eq", restaurant.id).or(orderStatus => orderStatus.status("eq", "NEW").status("eq", "COOKING").status("eq", "READY_FOR_PICKUP"))
+        );
         setOrders(orders);
     }
 
     useEffect(() => {
+        if(!restaurant) return;
         fetchOrders();
         const subscription = DataStore.observe(Order).subscribe(() => fetchOrders());
         return () => subscription.unsubscribe();
-    }, []);
+    }, [restaurant]);
 
     const renderOrderStatus = (orderStatus) => {
         let color = 'gray';
