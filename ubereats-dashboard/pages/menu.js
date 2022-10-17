59dd6b894e0cc69df255d627ab8a60ac177a7dc9
Rokas <rookas.rudzenskas@gmail.com>
2022-10-17 11:38:18 +0300

    Rokas

--- ubereats-dashboard/pages/menu.js
+++ ubereats-dashboard/pages/menu.js
@@ -1,11 +1,39 @@
 import React from 'react';
-import {Card, Table } from 'antd';
+import {Card, Table, Button } from 'antd';
 import dishes from '../assets/data/dashboard/dishes.json';
+import Link from "next/link";
 
 const Menu = ({}) => {
-    return (
-        <Card title={'Menu'} style={{margin: 20}}>
+    const tableColumns = [
+        {
+            title: 'Menu Item',
+            dataIndex: 'name',
+            key: 'name',
+        },
+        {
+            title: "Price",
+            dataIndex: 'price',
+            key: 'price',
+            render: (price) => <span>{price} $</span>
+        },
+        {
+            title: 'Action',
+            key: 'action',
+            render: () => <Button danger>Remove</Button>
+        }
+    ];
 
+    const renderNewItemButton = () => (
+        <Link href={'/menu/create'}>
+            <Button type={'primary'}>
+                New Item
+            </Button>
+        </Link>
+    )
+
+    return (
+        <Card title={'Menu'} style={{margin: 20}} extra={renderNewItemButton()}>
+            <Table dataSource={dishes} columns={tableColumns} rowKey={'id'} />
         </Card>
     );
 };
