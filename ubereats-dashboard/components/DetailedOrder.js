57ae42a5cc11f6b215c76ab9a7d892d38277845f
Rokas <rookas.rudzenskas@gmail.com>
2022-10-17 09:58:01 +0300

    Rokas

---
+++ ubereats-dashboard/components/DetailedOrder.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import {Button, Card, Descriptions, Divider, List} from "antd";
+import dishes from "../assets/data/dashboard/dishes.json";
+
+const DetailedOrder = ({}) => {
+    return (
+        <div>
+            <Card title={'Order Title'} style={{margin: 20}}>
+                <Descriptions bordered column={{lg: 1, md: 1, sm: 1}}>
+                    <Descriptions.Item label={'Customer'}>Rokas Rudzianskas</Descriptions.Item>
+                    <Descriptions.Item label={'Customer Address'}>Address 16, Nutella Street</Descriptions.Item>
+                </Descriptions>
+                <Divider />
+                <List dataSource={dishes} renderItem={(dishItem) => (
+                    <List.Item>
+                        <div style={{fontWeight: 'bold'}}>{dishItem?.name || 'Loading...'} x{dishItem?.quantity || 'Loading...'}</div>
+                        <div>${dishItem?.price || 'Loading...'}</div>
+                    </List.Item>
+                )} />
+                <Divider />
+                <div style={styles.totalSumContainer}>
+                    <h2>Total:</h2>
+                    <h2 style={styles.totalPrice}>$45.45</h2>
+                </div>
+                <Divider />
+                <div style={styles.buttonsContainer}>
+                    <Button block type={'danger'} size={'large'} style={styles.button}>
+                        Decline Order
+                    </Button>
+                    <Button block type={'primary'} size={'large'} style={styles.button}>
+                        Accept Order
+                    </Button>
+                </div>
+                <Button block type={'primary'} size={'large'}>
+                    Food Is Done
+                </Button>
+            </Card>
+        </div>
+    );
+};
+
+const styles = {
+    totalSumContainer: {
+        flexDirection: 'row',
+        display: 'flex',
+    },
+    totalPrice: {
+        marginLeft: 'auto',
+        fontWeight: 'bold'
+    },
+    buttonsContainer: {
+        display: 'flex',
+        paddingBottom: 30
+    },
+    button: {
+        marginRight: 20,
+        marginLeft: 20,
+    }
+}
+
+export default DetailedOrder;
+// by Rokas with ❤️
--- ubereats-dashboard/pages/index.js
+++ ubereats-dashboard/pages/index.js
@@ -1,7 +1,6 @@
 import Head from 'next/head'
-import {Card, Descriptions, Divider, List, Button} from 'antd';
-import dishes from '../assets/data/dashboard/dishes.json';
 import '../styles/Home.module.css';
+import DetailedOrder from "../components/DetailedOrder";
 const Home = () => {
 
     // does it work?
@@ -13,57 +12,10 @@ const Home = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <Card title={'Order Title'} style={{margin: 20}}>
-            <Descriptions bordered column={{lg: 1, md: 1, sm: 1}}>
-                <Descriptions.Item label={'Customer'}>Rokas Rudzianskas</Descriptions.Item>
-                <Descriptions.Item label={'Customer Address'}>Address 16, Nutella Street</Descriptions.Item>
-            </Descriptions>
-          <Divider />
-          <List dataSource={dishes} renderItem={(dishItem) => (
-              <List.Item>
-                    <div style={{fontWeight: 'bold'}}>{dishItem?.name || 'Loading...'} x{dishItem?.quantity || 'Loading...'}</div>
-                    <div>${dishItem?.price || 'Loading...'}</div>
-              </List.Item>
-          )} />
-          <Divider />
-          <div style={styles.totalSumContainer}>
-              <h2>Total:</h2>
-              <h2 style={styles.totalPrice}>$45.45</h2>
-          </div>
-          <Divider />
-          <div style={styles.buttonsContainer}>
-              <Button block type={'danger'} size={'large'} style={styles.button}>
-                  Decline Order
-              </Button>
-              <Button block type={'primary'} size={'large'} style={styles.button}>
-                  Accept Order
-              </Button>
-          </div>
-          <Button block type={'primary'} size={'large'}>
-              Food Is Done
-          </Button>
-      </Card>
+        <DetailedOrder />
     </div>
   )
 }
 
-const styles = {
-    totalSumContainer: {
-        flexDirection: 'row',
-        display: 'flex',
-    },
-    totalPrice: {
-        marginLeft: 'auto',
-        fontWeight: 'bold'
-    },
-    buttonsContainer: {
-        display: 'flex',
-        paddingBottom: 30
-    },
-    button: {
-        marginRight: 20,
-        marginLeft: 20,
-    }
-}
 
 export default Home;
