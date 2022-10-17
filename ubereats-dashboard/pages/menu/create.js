daa085409153b9b9b8f50e3a0c44a41689726d04
Rokas <rookas.rudzenskas@gmail.com>
2022-10-17 11:41:47 +0300

    Rokas

--- ubereats-dashboard/pages/menu/create.js
+++ ubereats-dashboard/pages/menu/create.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import {Form, Input, Button, Card} from 'antd';
 
 const Create = ({}) => {
     return (
-        <div>
-            
-        </div>
+        <Card title={'New Menu Item'} style={{margin: 20}}>
+
+        </Card>
     );
 };
 
