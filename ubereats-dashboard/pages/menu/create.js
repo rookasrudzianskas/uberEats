02631f9a1d3be1e78a7a10ab9d4eccfd28138a95
Rokas <rookas.rudzenskas@gmail.com>
2022-10-17 11:48:07 +0300

    Rokas

--- ubereats-dashboard/pages/menu/create.js
+++ ubereats-dashboard/pages/menu/create.js
@@ -1,10 +1,24 @@
 import React from 'react';
-import {Form, Input, Button, Card} from 'antd';
+import {Form, Input, Button, Card, InputNumber} from 'antd';
 
 const Create = ({}) => {
+    const {TextArea} = Input;
+
     return (
         <Card title={'New Menu Item'} style={{margin: 20}}>
+            <Form layout={'vertical'}>
+                <Form.Item label="Dish Name" required={true}>
+                    <Input placeholder={'Enter dish name'}/>
+                </Form.Item>
 
+                <Form.Item label="Dish Description" required={true}>
+                    <TextArea rows={4} placeholder={'Enter dish description'}/>
+                </Form.Item>
+
+                <Form.Item label="Dish Price ($)" required={true}>
+                    <InputNumber />
+                </Form.Item>
+            </Form>
         </Card>
     );
 };
