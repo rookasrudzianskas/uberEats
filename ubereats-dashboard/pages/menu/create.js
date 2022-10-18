2ef036a4412a24fa0534f0fd1562b7433432f7a6
Rokas <rookas.rudzenskas@gmail.com>
2022-10-18 09:35:44 +0300

    Rokas

--- ubereats-dashboard/pages/menu/create.js
+++ ubereats-dashboard/pages/menu/create.js
@@ -4,23 +4,38 @@ import {Form, Input, Button, Card, InputNumber} from 'antd';
 const Create = ({}) => {
     const {TextArea} = Input;
 
+    const onFinish = (values) => {
+        console.log('Success:', values);
+    }
+
+    const onFinishFailed = (errorInfo) => {
+
+    }
+
     return (
         <Card title={'New Menu Item'} style={{margin: 20}}>
-            <Form layout={'vertical'} wrapperCol={{span: 8}}>
-                <Form.Item label="Dish Name" required={true}>
+            <Form layout={'vertical'} wrapperCol={{span: 8}} onFinish={onFinish} onFinishFailed={onFinishFailed}>
+                <Form.Item
+                    rules={[{required: true, message: 'Please input menu item name'}]}
+                    name={'name'} label="Dish Name" required={true}>
                     <Input placeholder={'Enter dish name'}/>
                 </Form.Item>
 
-                <Form.Item label="Dish Description" required={true}>
+                <Form.Item
+                    rules={[{required: true, message: 'Please input menu item description'}]}
+                    name={'description'} label="Dish Description" required={true}>
                     <TextArea rows={4} placeholder={'Enter dish description'}/>
                 </Form.Item>
 
-                <Form.Item label="Dish Price ($)" required={true}>
+                <Form.Item
+                    rules={[{required: true, message: 'Please input menu item price'}]}
+                    type={'number'}
+                    name='price' label="Dish Price ($)" required={true}>
                     <InputNumber />
                 </Form.Item>
 
                 <Form.Item label="" required={true}>
-                    <Button type={'primary'}>Submit</Button>
+                    <Button type={'primary'} htmlType={'submit'}>Submit</Button>
                 </Form.Item>
             </Form>
         </Card>
