937fc1201aa47c2e772ca65b06432ece411b8900
Rokas <rookas.rudzenskas@gmail.com>
2022-10-18 09:28:20 +0300

    Rokas

--- ubereats-dashboard/pages/order-history.js
+++ ubereats-dashboard/pages/order-history.js
@@ -4,10 +4,12 @@ import orders from "../assets/data/dashboard/orders-history.json";
 import {useRestaurantContext} from "../contexts/RestaurantContext";
 import {Order, OrderStatus} from "../src/models";
 import {DataStore} from "aws-amplify";
+import {useRouter} from "next/router";
 
 const OrderHistory = ({}) => {
     const [orders, setOrders] = useState([]);
     const {restaurant} = useRestaurantContext();
+    const router = useRouter();
 
     const fetchOrders = async () => {
         const orders = await DataStore.query(Order, (order) =>
@@ -65,7 +67,7 @@ const OrderHistory = ({}) => {
                 columns={tableColumns}
                 rowKey={'id'}
                 onRow={(orderItem) => ({
-                    // onClick: () => router.push(`/order/${orderItem?.orderID}`)
+                    onClick: () => router.push(`/order/${orderItem?.orderID}`)
                 })}
             />
         </Card>
